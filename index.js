9ed336eef60c3ab4b3072166f6ad3d1d1aa24258
Aditi Ohri <didiohri@gmail.com>
2019-07-25 02:02:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
+  var newObj = deleteFromObjectByKey({}, object); 
   delete newObj.key;
   return newObj;
 }
