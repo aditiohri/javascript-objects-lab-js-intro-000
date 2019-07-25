3331b106047f4c9f912fee02fa2c6e7009220a7d
Aditi Ohri <didiohri@gmail.com>
2019-07-25 01:49:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ almondmilk: '1 cup',
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]:value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]: value;
+  return object;
+}
