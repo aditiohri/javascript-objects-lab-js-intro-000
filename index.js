0d539355a731255924db326c05b1e324dfe19744
Aditi Ohri <didiohri@gmail.com>
2019-07-25 01:42:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {
 oats: '1 cup',
 raspberries: '1 cup',
 flaxseeds: '1 cup',
-almond milk: '1 cup',
+almondmilk: '1 cup',
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
